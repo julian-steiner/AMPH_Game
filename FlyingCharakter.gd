9e038fc148b7011bd843c375077508a4988bfaed
SchwammPIzza <ambros.anrig@gmail.com>
2020-12-10 21:24:12 +0100

    Skript Flying Charakter

--- FlyingCharakter.gd
+++ FlyingCharakter.gd
@@ -1,16 +1,20 @@
+class_name FlyingCharakter
 extends Node2D
 
+const upflight_max = 200;
+const upflight_acc = 80;
+const xflight_max = 200;
+const xflight_acc = 80;
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _integrate_forces(state):
+	var velocity = state.get_linear_velocity()
+	var step = state.get_step()
+	var on_floor = false
+	
+	for i in range(state.get_contact_count()):
+		var contact_normal = state.get_contact_local(i)
+		if contact_normal.dot(Vector2(0,-1)) > 0.5:
+			on_floor = true
+	
+	if Input.is_action_just_pressed("NumPad8"):
+		pass
