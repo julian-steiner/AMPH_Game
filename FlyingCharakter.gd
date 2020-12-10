2374d611eb37e5afe533b5e8ce61248383e12ed9
SchwammPIzza <ambros.anrig@gmail.com>
2020-12-10 23:43:50 +0100

    Skript Flying Charakter

--- FlyingCharakter.gd
+++ FlyingCharakter.gd
@@ -1,8 +1,9 @@
 class_name FlyingCharakter
-extends Node2D
+
+extends RigidBody2D
 
 const upflight_max = 200;
-const upflight_acc = 80;
+#const upflight_acc = 80;
 const xflight_max = 200;
 const xflight_acc = 80;
 
@@ -16,5 +17,32 @@ func _integrate_forces(state):
 		if contact_normal.dot(Vector2(0,-1)) > 0.5:
 			on_floor = true
 	
-	if Input.is_action_just_pressed("NumPad8"):
-		pass
+	if Input.is_action_just_pressed("ui_KP 8"):
+		velocity.y -= upflight_max
+	
+	if Input.is_action_just_pressed("ui_KP 4") and velocity.y != 0:
+		velocity.x = min(velocity.x - xflight_acc*step, -xflight_max)
+	if Input.is_action_pressed("ui_KP 4") and velocity.y == 0:
+		velocity.x = min(velocity.x - xflight_acc*step, -xflight_max)
+	
+	if Input.is_action_just_pressed("ui_KP 6") and velocity.y != 0:
+		velocity.x = max(velocity.x + xflight_acc*step, xflight_max)
+	elif Input.is_action_pressed("ui_KP 6") and velocity.y == 0:
+		velocity.x = max(velocity.x + xflight_acc*step, xflight_max)
+	
+	if Input.is_action_just_pressed("ui_KP enter"):
+		$AnimatedSprite.play("Attak")
+	
+	
+	if velocity.y != 0:
+		$AnimatedSprite.play("Fly")
+	else:
+		$AnimatedSprite.play("Idle")
+	
+	if velocity.x > 0:
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+	
+	velocity += state.get_total_gravity() * step
+	state.set_linear_velocity(velocity)
