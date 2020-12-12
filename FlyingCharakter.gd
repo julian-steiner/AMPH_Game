7a6098815824ba581ba38e15baac7c7d100542f9
SchwammPIzza <ambros.anrig@gmail.com>
2020-12-12 16:06:58 +0100

    Behebung des unflüssiges rutschen des Bats

--- FlyingCharakter.gd
+++ FlyingCharakter.gd
@@ -3,9 +3,9 @@ class_name FlyingCharakter
 extends RigidBody2D
 
 const upflight_max = 200;
-#const upflight_acc = 80;
-const xflight_max = 200;
-const xflight_acc = 80;
+const downflight_max = -100;
+const xflight_max = 300;
+const xflight_acc = 120;
 
 func _integrate_forces(state):
 	var velocity = state.get_linear_velocity()
@@ -19,30 +19,36 @@ func _integrate_forces(state):
 	
 	if Input.is_action_just_pressed("ui_KP 8"):
 		velocity.y -= upflight_max
+		
+	if Input.is_action_just_pressed("ui_KP 5"):
+		velocity.y -= downflight_max
 	
-	if Input.is_action_just_pressed("ui_KP 4") and velocity.y != 0:
+	if Input.is_action_just_pressed("ui_KP 4"):
 		velocity.x = min(velocity.x - xflight_acc*step, -xflight_max)
-	if Input.is_action_pressed("ui_KP 4") and velocity.y == 0:
-		velocity.x = min(velocity.x - xflight_acc*step, -xflight_max)
-	
-	if Input.is_action_just_pressed("ui_KP 6") and velocity.y != 0:
+#	if Input.is_action_pressed("ui_KP 4") and velocity.y == 0:
+#		velocity.x = min(velocity.x - xflight_acc*step, -xflight_max)
+#
+	if Input.is_action_just_pressed("ui_KP 6"):
 		velocity.x = max(velocity.x + xflight_acc*step, xflight_max)
-	elif Input.is_action_pressed("ui_KP 6") and velocity.y == 0:
-		velocity.x = max(velocity.x + xflight_acc*step, xflight_max)
+#	if Input.is_action_pressed("ui_KP 6") and velocity.y == 0:
+#		velocity.x = max(velocity.x + xflight_acc*step, xflight_max)
 	
 	if Input.is_action_just_pressed("ui_KP enter"):
 		$AnimatedSprite.play("Attak")
+		print("Attak")
 	
 	
 	if velocity.y != 0:
 		$AnimatedSprite.play("Fly")
+	
 	else:
 		$AnimatedSprite.play("Idle")
 	
-	if velocity.x > 0:
-		$AnimatedSprite.flip_h = false
-	else:
-		$AnimatedSprite.flip_h = true
+	if velocity.x != 0:
+		if velocity.x > 0:
+			$AnimatedSprite.flip_h = false
+		else:
+			$AnimatedSprite.flip_h = true
 	
 	velocity += state.get_total_gravity() * step
 	state.set_linear_velocity(velocity)
