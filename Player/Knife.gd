b63bf308f37287f55f1efd3cf5f6529edccb1f46
Julian Steiner <d>
2021-01-09 10:53:48 +0100

    added scaled spikes

--- Player/Knife.gd
+++ Player/Knife.gd
@@ -11,16 +11,14 @@ func _integrate_forces(state):
 		self.set_gravity_scale(1.0);
 
 func _on_knife_collision_body_entered(body): 
-	print("Collision")
 	if body is Enemy and not on_floor:
 		body.hp -= 100;
 		self.linear_velocity = Vector2(0, 0)
+		self.angular_velocity = 0
 	if not body is Character and not body == self and not body is Enemy:
 		on_floor = true;
 
 func _on_pickup_collision_body_entered(body):
-	print("Collision")
 	if body is Character and on_floor:
 		body.knives += 1;
-		print("Removed Knife")
 		queue_free()
