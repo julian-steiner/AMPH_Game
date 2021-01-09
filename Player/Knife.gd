58fd746317401bd64f66ad8c8fd458e166b6f927
Julian Steiner <d>
2021-01-09 10:26:39 +0100

    bugfixes of character not dying

--- Player/Knife.gd
+++ Player/Knife.gd
@@ -4,19 +4,22 @@ extends RigidBody2D
 var on_floor = false;
 
 func _integrate_forces(state):
-	
+	print(state.angular_velocity)
 	if on_floor:
 		state.linear_velocity = Vector2(0, 0)
 		state.angular_velocity = 0
 		self.set_gravity_scale(1.0);
 
 func _on_knife_collision_body_entered(body): 
+	print("Collision")
 	if body is Enemy and not on_floor:
 		body.hp -= 100;
 	if not body is Character and not body == self:
 		on_floor = true;
-		
+
 func _on_pickup_collision_body_entered(body):
+	print("Collision")
 	if body is Character and on_floor:
 		body.knives += 1;
+		print("Removed Knife")
 		queue_free()
