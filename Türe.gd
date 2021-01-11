a45532dd56d97b4420f9338098a8ad55400fac77
SchwammPizza <ambros.anrig@gmail.com>
2021-01-11 12:09:53 +0100

    Türe

--- Türe.gd
+++
@@ -1,9 +0,0 @@
-class_name Tuere
-extends Area2D
-
-var x = 0
-var y = 0
-
-func _on_Tuere_body_entered(body):
-	if body is Character or body is FlyingCharakter:
-		body.transform[2] = Vector2(x,y)
---
+++ Türen/Türe.gd
@@ -0,0 +1,9 @@
+class_name Tuere
+extends StaticBody2D
+
+var x = 300
+var y = 0
+
+func _on_Area2D_body_entered(body):
+	if body is Character or body is FlyingCharakter:
+		body.transform.origin = Vector2(x,y)
