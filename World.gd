eb39a371648ab7f0c1d98fffe37d62ba1db1b42a
SchwammPizza <ambros.anrig@gmail.com>
2021-01-18 12:12:55 +0100

    Level1

--- World.gd
+++ World.gd
@@ -5,4 +5,4 @@ extends Node2D
 
 func _on_Area2D_body_entered(body):
 	if body is Character:
-		get_tree().change_scene(("res://Level_1.tscn"))
+		get_tree().change_scene(("res://Levels//Level_1.tscn"))
