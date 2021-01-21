91382e7a4a10384d7c25c2435e9a9b0640e7f17c
Julian Steiner <juli.steiner@bluewin.ch>
2021-01-21 14:22:50 +0100

    added level 2 and physically correct knife

--- Player/Dagger.gd
+++ Player/Dagger.gd
@@ -1,8 +1,6 @@
 class_name Dagger
 extends Area2D
 
-
-
 func _on_Hitbox_body_entered(body):
 	if body is Enemy:
 		body.hp -= 20
--- Player/Knife.gd
+++ Player/Knife.gd
@@ -2,12 +2,30 @@ class_name knife_flying
 extends RigidBody2D
 
 var on_floor = false;
+var velocity = Vector2(0, 0);
+var angle = 0;
+const m = 1;
+
+func to_degrees(angle):
+	return angle / (2 * PI) * 360 + 180
+
+func to_radians(angle):
+	return (angle - 180) / 360 * 2 * PI 
+
+func calculate_friction():
+	return 0.5 * (0.000678 * sin(0.03605 * angle * (-1.71782)) + 0.00154) * 1.2 * pow(velocity.x, 2)
 
 func _integrate_forces(state):
+	velocity = state.get_linear_velocity();
+	angle = to_degrees(state.get_transform().get_rotation());
+	print(calculate_friction())
 	if on_floor:
 		state.linear_velocity = Vector2(0, 0)
 		state.angular_velocity = 0
 		self.set_gravity_scale(1.0);
+	else:
+		state.linear_velocity = Vector2(300, state.linear_velocity.y)
+		state.linear_velocity.y -= calculate_friction()
 
 func _on_knife_collision_body_entered(body): 
 	if body is Enemy and not on_floor:
