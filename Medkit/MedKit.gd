a5aa90c9c35a478fb12fddfffa097e69e610a002
SchwammPIzza <ambros.anrig@gmail.com>
2021-01-25 20:24:37 +0100

    Overworkt Medkit, Level_1 dekoration in BG

--- Medkit/MedKit.gd
+++ Medkit/MedKit.gd
@@ -3,4 +3,6 @@ extends StaticBody2D
 
 func _on_Area2D_body_entered(body):
 	if body is Character or body is FlyingCharakter:
-		body.hp = 100
+		if body.hp != 100 and not body.hp <= 0:
+			body.hp = 100
+			queue_free()
