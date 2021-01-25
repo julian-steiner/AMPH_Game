0f47edf7fa581216ad651f2a002deaac821c2ede
Julian Steiner <juli.steiner@bluewin.ch>
2021-01-25 10:53:04 +0100

    Moved some files to folders

--- MedKit.gd
+++
@@ -1,6 +0,0 @@
-class_name MedKit
-extends StaticBody2D
-
-func _on_Area2D_body_entered(body):
-	if body is Character or body is FlyingCharakter:
-		body.hp = 100
--- Medkit/MedKit.gd
+++ Medkit/MedKit.gd
@@ -4,4 +4,3 @@ extends StaticBody2D
 func _on_Area2D_body_entered(body):
 	if body is Character or body is FlyingCharakter:
 		body.hp = 100
-		queue_free()
