3479f0913347f4a354d9e98747f3cd4a3137ef7b
Julian Steiner <d>
2021-01-26 21:57:07 +0100

    Added buttons

--- UserInterface/UI.gd
+++ UserInterface/UI.gd
@@ -8,14 +8,28 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var config = File.new()
+	config.open("res://game_information.save", File.WRITE);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+
 var level_number = 1
 
 func _on_Start_Button_pressed():
 	get_tree().change_scene("res://Levels/Charakter_Levels/Level_" + str(level_number) + ".tscn")
+
+
+func _on_Continue_Button_pressed():
+	pass # Replace with function body.
+
+
+func _on_Assassin_select_Button_pressed():
+	pass # Replace with function body.
+
+
+func _on_Bat_select_Button_pressed():
+	pass # Replace with function body.
