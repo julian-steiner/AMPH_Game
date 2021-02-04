8bad57426f47d0b5b3ebeac32bae8f771098511b
SchwammPIzza <ambros.anrig@gmail.com>
2021-02-04 23:00:49 +0100

    Add Charakter Level_4

---
+++ Levels/Character_Levels/Level_4.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var level_assassin = 4
+var level_bat = 0
+var c_character = 0
+
+func save_data():
+	var c_file = File.new();
+	c_file.open("res://game_information.save", File.WRITE)
+	c_file.store_line(to_json(level_assassin))
+	c_file.store_line(to_json(level_bat))
+	c_file.store_line(to_json(c_character))
+	c_file.close()
+	
+func load_data():
+	var c_file = File.new();
+	c_file.open("res://game_information.save", File.READ)
+	var level_assassin = int(c_file.get_line())
+	var level_bat = int(c_file.get_line())
+	var c_character = c_file.get_line().replace("\"", "");
+	c_file.close()
+	return {"level_assassin": level_assassin, "level_bat": level_bat, "c_character": c_character}
+
+func dynamic_zooming():
+	var window_size = OS.window_size
+	get_node("Character/Character/Camera2D").zoom = Vector2(1024 / window_size.x  , 600 / window_size.y)
+
+func _ready():
+	var data = load_data();
+	level_bat = data.get("level_bat");
+	c_character = data.get("c_character");
+	print(c_character)
+	save_data();
+	dynamic_zooming()
